1ade59f43fc5fb1005d2edfe648b38c34a8eb83a
tkhubert <tkhubert@gmail.com>
2014-10-19 16:05:47 +0100

    Initial Commit

---
+++ MiningMassiveDataSetsC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiningMassiveDataSetsC
+//
+//  Created by Thomas Hubert on 19/10/2014.
+//  Copyright (c) 2014 Thomas Hubert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
